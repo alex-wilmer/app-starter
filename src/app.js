e7bf0540f614b16b1f4a6a7343701518302eddca
Alex Wilmer <djazium@gmail.com>
2015-12-01 21:20:34 -0500

    reddit-images

--- src/app.js
+++ src/app.js
@@ -1,30 +1,84 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
-
-let App = () =>
+let ImagesList = ({ images = [] }) =>
   <div>
-    <h1>Hello world!</h1>
-    <Tabs>
-      <TabList>
-        <Tab>Foo</Tab>
-        <Tab>Bar</Tab>
-        <Tab>Baz</Tab>
-      </TabList>
-
-      <TabPanel>
-        <h2>Hello from Foo</h2>
-      </TabPanel>
-
-      <TabPanel>
-        <h2>Hello from Bar</h2>
-      </TabPanel>
-      
-      <TabPanel>
-        <h2>Hello from Baz</h2>
-      </TabPanel>
-    </Tabs>
+    { images.map(
+        image =>
+          <img src={ image.data.thumbnail } />
+      )
+    }
   </div>
 
-render(<App />, document.getElementById('app'))
+let NamesList = ({ names }) =>
+  <ul>
+    { names.map(
+        name =>
+          <li>Name: { name }</li>
+      )
+    }
+  </ul>
+
+let SeafoamButton = ({ handleClick, message }) =>
+  <button
+    onClick={ () => handleClick('bananas') }
+    style={{
+      fontSize: '3em',
+      background: 'rgb(50, 223, 141)'
+    }}
+  >
+    { message }
+  </button>
+
+class App extends React.Component {
+  constructor () {
+    super()
+
+    this.state = {
+      message: 'Hello message!!!!',
+      names: [
+        'alice', 'bob', 'joe'
+      ]
+    }
+  }
+
+  getImages = () => {
+    fetch('https://www.reddit.com/r/toronto.json')
+      .then(response => response.json())
+      .then(({data}) => {
+        console.log(data.children)
+
+        this.setState({ images: data.children })
+      })
+  }
+
+  changeMessage = (value) => {
+    this.setState({ message: value })
+  }
+
+  render () {
+    return (
+      <div>
+        <SeafoamButton
+          message={ this.state.message }
+          handleClick={ this.changeMessage }
+        />
+
+        <NamesList
+          names={ this.state.names }
+        />
+
+        <ImagesList
+          images={ this.state.images }
+        />
+
+        <button onClick={ this.getImages }>get images</button>
+      </div>
+    )
+  }
+}
+
+render(
+  <App />,
+  document.getElementById('app')
+)
